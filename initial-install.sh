5267eafc93c9ed6b77e98ded32820f18bc5ba4b6
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2018-03-07 14:33:22 -0600

    Added initial install script

---
+++ initial-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Install Xcode Command Line Tools.
+xcode-select --install
+
+# Install Homebrew.
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ansible
+brew install autojump
+brew install awscli
+brew install bash-completion
+brew install brew-cask
+brew install ctags
+brew install git
+brew install go
+brew install googler
+brew install htop
+brew install htop-osx
+brew install httpie
+brew install irssi
+brew install jq
+brew install lynx
+brew install macvim
+brew install mysql
+brew install neofetch
+brew install netcat
+brew install node
+brew install nvm
+brew install pdsh
+brew install pyenv
+brew install python
+brew install sngrep
+brew install sqlite
+brew install thefuck
+brew install tig
+brew install tmux
+brew install watch
+brew install wget
