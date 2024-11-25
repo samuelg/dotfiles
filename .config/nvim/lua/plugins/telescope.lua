42dbafed5029ee9ef51cd1132ecb0f38aef2f94b
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-11-24 19:45:01 -0600

    Tweaks to neovim config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -51,11 +51,14 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = {
+            -- ['<c-enter>'] = 'to_fuzzy_refine',
+            ['<esc>'] = require('telescope.actions').close,
+          },
+        },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
