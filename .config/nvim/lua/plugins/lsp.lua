f16a07b61ff0261f28437c2f85586f4588ab2772
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-11-24 19:53:27 -0600

    Added go language server for neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -166,7 +166,7 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      -- gopls = {},
+      gopls = {},
       pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -176,7 +176,6 @@ return {
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {},
-      --
 
       lua_ls = {
         -- cmd = {...},
