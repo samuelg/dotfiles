43be20ae3722b36bf77ee2689c084ed196c59591
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-12-04 15:17:15 -0600

    Neovim config tweaks

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -33,43 +33,43 @@ o.foldlevel = 99
 o.foldlevelstart = 1
 
 -- Enable break indent
-vim.opt.breakindent = true
+o.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+o.ignorecase = true
+o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+o.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
+o.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+o.timeoutlen = 1000
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+o.splitright = true
+o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+o.list = true
+o.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+o.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+o.scrolloff = 10
 
 -- ----------- Key mappings -----------
 -- NOTE: some key mappings configured in plugin specs
