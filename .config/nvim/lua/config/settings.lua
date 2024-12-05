3b3852a923e24ae804e3000e26e8dc5bad5a28d6
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-12-05 10:56:01 -0600

    Increased minimum fold level

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -30,7 +30,7 @@ o.foldmethod = "expr"
 o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 o.foldtext = ""
 o.foldlevel = 99
-o.foldlevelstart = 1
+o.foldlevelstart = 3
 
 -- Enable break indent
 o.breakindent = true
