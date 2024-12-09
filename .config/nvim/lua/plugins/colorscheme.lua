78650fd8e3fb93025a56e63316a19b17a84d0377
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-12-09 09:40:32 -0600

    Changes color scheme across alacritty, neovim, and zellij

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-    "morhetz/gruvbox",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme gruvbox")
-    end,
+  "oxfist/night-owl.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    -- load the colorscheme here
+    require("night-owl").setup()
+    vim.cmd.colorscheme("night-owl")
+  end,
 }
