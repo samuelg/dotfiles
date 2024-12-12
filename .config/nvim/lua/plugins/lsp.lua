9777a5a468eb1b66906ff988014ef617c9087a80
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2024-12-12 12:34:33 -0600

    Tweaked neovim bindings for diagnostics

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -93,6 +93,8 @@ return {
         -- or a suggestion from your LSP for this to activate.
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
+        map('<leader>y', vim.diagnostic.open_float, 'Show diagnostic floating window')
+
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
